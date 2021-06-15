7bd80aee2eb33bded8aa47fb1c47a24d2a06b068
aditya muley <aditya3muley@gmail.com>
2021-06-15 09:34:24 +0530

    fname flag description changes

---
+++ app/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"flag"
+	"io/ioutil"
+	"log"
+	"strings"
+
+	"github.com/link"
+)
+
+var fileName *string
+
+func init() {
+	fileName = flag.String("fname", "ex1.txt", "HTML File containing html tags")
+	flag.Parse()
+}
+
+func readHtmlFile(str string) []byte {
+	content, err := ioutil.ReadFile(str)
+	if err != nil {
+		log.Printf("HTML File.Get err   #%v ", err)
+	}
+
+	return content
+}
+
+func main() {
+	htm := readHtmlFile(*fileName)
+	html := strings.NewReader(string(htm))
+	links, err := link.ParseHtml(html)
+	if err != nil {
+		panic(err)
+	}
+	link.Display(links)
+}
